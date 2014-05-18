ede49b49355c81b6ebe33842c0f721ded3ac6e66
danielemoro <daniele1.moro@mail.polimi.it>
2014-05-18 12:36:21 +0200

    Modifica dei commenti della classe StatoPartita e TipoTerreno per JavaDoc; aggiunta del "vettore" delle tessere in stato partita con metodo per l'init, aggiunta metodo static in TipoTerreno che ritorna l'elenco di tutte le possibili istanze di TipoTerreno

--- iodice_moro/src/main/java/it/polimi/iodice_moro/model/StatoPartita.java
+++ iodice_moro/src/main/java/it/polimi/iodice_moro/model/StatoPartita.java
@@ -17,87 +17,155 @@ import org.jgrapht.graph.SimpleGraph;
 
 
 //PER ORA implementa solo il caricamento della mappa da file xml (file che si trova nella stessa cartella della classe
+/**
+ * Classe che gestisce lo stato corrente della partita.
+ * SINGLETON
+ * @author Antonio Iodice, Daniele Moro
+ *
+ */
 public class StatoPartita {
 	
+	/**
+	 * Costante per il numero di recinti normali massimo
+	 */
+	private static final int NUM_RECINTI_MAX = 20;
+
+	/**
+	 * Costante per il file da cui caricare il grafo
+	 */
 	private static final String FILE_GRAFO = "grafo.xml";
 	
-	//ATTRIBUTI
+	/**
+	 * Numero di recinti ancora da posizionare
+	 */
 	public int numRecinti;
+	
+	/**
+	 * Verifica se si è o meno nel turno finale
+	 */
 	public boolean turnoFinale;
 	
-	//Attributo per gestire l'istanza singleton.
+	/**
+	 * Tessere per la gestione del costo dei terreni
+	 */
+	Map<String,Integer> tessere= new HashMap<String, Integer>();
+	
+	/**
+	 * Grafo che rappresenta la mappa, i vertici del grafo sono sia strade che regioni,
+	 * gli archi sono tra Regioni e Strade (regioni che hanno come confine la strada) e tra strade(tra strade adiacenti)
+	 */
+	private UndirectedGraph<VerticeGrafo,DefaultEdge> mappa;
+	
+	/**
+	 * Lista delle strade della mappa
+	 */
+	private ArrayList<Strada> strade= new ArrayList<Strada>();
+	
+	/**
+	 * Lista delle regioni della mappa
+	 */
+	private ArrayList<Regione> regioni= new ArrayList<Regione>();
+	
+	/**
+	 * Attributo per gestire l'istanza singleton.
+	 */
 	private static StatoPartita instance = null;
 	
-	//Costruttore fittizio. Prima di chiamare il costruttore controlla 
-	//se esiste già istanza di StatoPartita.
+	
+	
+	/**
+	 * Metodo per instanziare l'unico oggetto che può essere presente.
+	 * Prima di chiamare il costruttore controlla se esiste già una istanza di StatoPartita
+	 * @return Ritorna l'unica istanza della classe StatoPartita
+	 */
 	public static StatoPartita getInstance() {
 		if (instance == null)
 			instance = new StatoPartita();
 		return instance;
-			}
+	}
 	
-	
-	
-	//Grafo che rappresenta la mappa, i vertici del grafo sono sia strade che regioni
-	//gli archi sono tra Regioni e Strade (regioni che hanno come confine la strada) e tra strade(tra strade adiacenti)
-	private UndirectedGraph<VerticeGrafo,DefaultEdge> mappa;
-	private ArrayList<Strada> strade= new ArrayList<Strada>();
-	private ArrayList<Regione> regioni= new ArrayList<Regione>();
-	
-	//parser del file XML contenente la mappa
+	/**
+	 * parser del file XML contenente la mappa
+	 * @param pathFile Path del file da caricare
+	 */
 	private void parseMappaXML(String pathFile){
 		Map<Integer,VerticeGrafo> nodi = new HashMap<Integer, VerticeGrafo>();
 		try{
-			//SAXBuilder usato per creare oggetti JDOM2
+			/*
+			 * SAXBuilder usato per creare oggetti JDOM2
+			 */
 			SAXBuilder jdomBuilder = new SAXBuilder();
-			
-			//Apertura del file xml in cui e' contenuta la mappa
+			/*
+			 * Apertura del file xml in cui e' contenuta la mappa
+			 */
 			File xmlFile = new File(pathFile);
-
-			//jdomDocument e' l'oggetto JDOM2
+			/*
+			 * jdomDocument e' l'oggetto JDOM2
+			 */
 			Document jdomDocument = (Document) jdomBuilder.build(xmlFile);
-			//Prelevo il nodo ROOT
+			/*
+			 * Prelevo il nodo ROOT
+			 */
 			Element rootNode=jdomDocument.getRootElement();
-
-			//itero sulle regioni
+			
+			/*
+			 * Itero sulle regioni
+			 */
 			for (Element i : rootNode.getChildren("regione")){
+				/*
+				 * Prelevo i dati memorizzati nell'xml
+				 */
 				int id= Integer.parseInt(i.getAttributeValue("id"));
 				String tipo = i.getAttributeValue("tipo");
-				//Istanzio la nuova regione
+				/*
+				 * Istanzio la nuova classe Regione
+				 */
 				Regione nuovaReg= new Regione(tipo);
-				//la aggiungo alla hashmap dei nodi
+				/*
+				 * Aggiugo l'istanza alla HashMap dei nodi e al vettore delle regioni
+				 */
 				nodi.put(id,nuovaReg);
 				regioni.add(nuovaReg);
-				//la aggiungo al grafo
+				/*
+				 * Aggingo l'istanza al grafo (come vettore
+				 */
 				mappa.addVertex(nuovaReg);
 			}
-
-			//itero sulle strade
+			/*
+			 * Itero sulle starde e applico lo stesso ragionamento delle regioni
+			 */
 			for (Element i : rootNode.getChildren("strada")){
+				/*
+				 * Prelevo i dati memorizzati nell'xml
+				 */
 				int id= Integer.parseInt(i.getAttributeValue("id"));
 				int ncas = Integer.parseInt(i.getAttributeValue("ncasella"));
-				//Istanzio la nuova strada
+				
 				Strada nuovaStr= new Strada(ncas);
-				//la aggiungo alla hashmap dei nodi
 				nodi.put(id,nuovaStr);
 				strade.add(nuovaStr);
-				//la aggiungo al grafo
 				mappa.addVertex(nuovaStr);
 			}
-
-
-			//itero sui link tra strade
+			/*
+			 * itero sui link tra strade
+			 */
 			for (Element i : rootNode.getChildren("arco")){
 		//		int id= Integer.parseInt(i.getAttributeValue("id")); //ID dell'arco non usato
-				//Memorizzo id del source e della destinazione
+				/*
+				 * Memorizzo id del source e della destinazione
+				 */
 				int idSource = Integer.parseInt(i.getAttributeValue("source"));
 				int idDest = Integer.parseInt(i.getAttributeValue("dest"));
 				
-				//prelevo dalla hashmap gli oggetti correlati a quell'id
+				/*
+				 * prelevo dalla hashmap gli oggetti correlati a quell'id
+				 */
 				VerticeGrafo nodoSource = nodi.get(idSource);
 				VerticeGrafo nodoDest = nodi.get(idDest);
 				
-				//Aggiungo al grafo l'arco corrispondente
+				/*
+				 * Aggiungo al grafo l'arco corrispondente
+				 */
 				mappa.addEdge((VerticeGrafo)nodoSource, (VerticeGrafo)nodoDest);
 			}
 
@@ -119,60 +187,94 @@ public class StatoPartita {
 	private void caricaGrafo(){
 		parseMappaXML(FILE_GRAFO);
 	}
-	
-	
-	public StatoPartita() {
-		numRecinti=0;
+
+	/**
+	 * Costruttore per l'inizializzazione della partita, con caricamento della mappa,
+	 *  è privato perchè lo stato partita è un sigleton, per inizializare la classe si chiama instance()
+	 */
+	private StatoPartita() {
+		initTessere();
+		numRecinti=NUM_RECINTI_MAX;
 		turnoFinale=false;
 		mappa= new SimpleGraph<VerticeGrafo,DefaultEdge>(DefaultEdge.class);
 		caricaGrafo();
 	}
 	
-	//GETTERS & SETTERS	
+	/**
+	 * Metodo per inizializzare la lista di tessere con il loro costo,
+	 * il costo è inializzato a 0
+	 */
+	private void initTessere(){
+		TipoTerreno[] tipi= TipoTerreno.getTipi();
+		for(TipoTerreno t : tipi){
+			tessere.put(t.toString(), 0);
+		}
+	}
+	
+	/**
+	 * @return Numero di recinti ancora disponibili
+	 */
 	public int getNumRecinti() {
 		return numRecinti;
 	}
-
-	public void setNumRecinti(int numRecinti) {
-		this.numRecinti = numRecinti;
+	
+	/**
+	 * Metodo che decrementa il numero di recinti, quando uno di questi viene utilizzato
+	 */
+	public void decrementaNumRecinti() {
+		this.numRecinti--;
 	}
 
+	/**
+	 * @return Ritorna lo stato del turno finale
+	 */
 	public boolean isTurnoFinale() {
 		return turnoFinale;
 	}
-
+	
+	/**
+	 * @param turnoFinale Per settare quando è il turno finale
+	 */
 	public void setTurnoFinale(boolean turnoFinale) {
 		this.turnoFinale = turnoFinale;
 	}
 
-	//Metodo che ritorna le strade adiacenti ad una strada data
+	/**
+	 * Metodo che ritorna le strade adiacenti ad una starda data
+	 * @param strada Strada di cui si vuole trovare le strade adiacenti
+	 * @return Ritorna le Strade adiacenti
+	 */
 	private ArrayList<Strada> getStradeAdiacenti(Strada strada){
-		//Prelevo tutti gli archi che partono dalla strada di cui devo trovare le adiacenti
+		/*
+		 * Prelevo tutti gli archi che partono dalla strada di cui devo trovare le adiacenti
+		 */
 		Set<DefaultEdge> archi =mappa.edgesOf(strada);
 		ArrayList<Strada> stradeAdiacenti = new ArrayList<Strada>();
 		
-		//itero sugli archi ottenuti
+		/*
+		 * itero sugli archi ottenuti, per ogni arco prelevo la sorgente dell'arco e 
+		 * controllo che il vertice sia una regione e non sia la strada che ho come parametro; 
+		 * se è una regione la memorizzo nell'arrai delle starde adiacenti
+		 */
 		for(DefaultEdge arc : archi){
-			//per ogni arco prelevo la sorgente dell'arco
 			VerticeGrafo destArco = mappa.getEdgeSource(arc);
-			//controllo che il vertice sia una regione
 			if(!destArco.isRegione() && destArco!=strada){
-				//Se e' una regione la memorizzo nell'array delle strade adiacenti
 				stradeAdiacenti.add((Strada) destArco);
 			}
-			
-			//stesso ragionamento di prima solo per la destinazione dell'arco
+			/*
+			 * stesso ragionamento di prima solo per la destinazione dell'arco
+			 */
 			destArco = mappa.getEdgeTarget(arc);
 			if(!destArco.isRegione() && destArco!=strada){
 				stradeAdiacenti.add((Strada)destArco);
 			}	
 		}
-		
 		return stradeAdiacenti;
 	}
+	
 	public static void main(String[] args){
 		//Test per verificare il caricamento della mappa da XML e il prelievo delle strade adiacenti
-		StatoPartita stato = new StatoPartita();
+		StatoPartita stato = getInstance();
 		System.out.println("Adiacenze"+stato.getStradeAdiacenti(stato.strade.get(0)).toString());
 	}
 	
--- iodice_moro/src/main/java/it/polimi/iodice_moro/model/TipoTerreno.java
+++ iodice_moro/src/main/java/it/polimi/iodice_moro/model/TipoTerreno.java
@@ -1,20 +1,43 @@
 package it.polimi.iodice_moro.model;
 
 
+/**
+ * Enumeratore per il tipo del terreno della mappa
+ * @author Antonio Iodice, Daniele Moro
+ *
+ */
+
 public enum TipoTerreno {
 SABBIA, MONTAGNA, PIANURA, BOSCO, PALUDI, COLTIVAZIONI, SHEEPSBURG;
-	
-	// una specie di costruttore che mi serve per instanziare l'enumeratore corretto
-	public static TipoTerreno parseInput(String input){
-		return Enum.valueOf(TipoTerreno.class,  input.toUpperCase());  //Porto la stringa di input in maiuscolo, il metodo valueOf confronta il secondo parametro con tutte le possibili istanze dell'enumeratore, se non matcha con nessuna della possibilita mi torna un eccezione di tipo illegal argument exception
+
+	/**
+	 * 
+	 * @param input Stringa con il valore che deve assumere l'enum
+	 * @return Istanza dell'enumeratore corretta
+	 * @throws IllegalArgumentException
+	 */
+	public static TipoTerreno parseInput(String input) throws IllegalArgumentException{
+		/*
+		 * Porto la stringa di input in maiuscolo, il metodo valueOf confronta il secondo parametro con tutte le possibili istanze dell'enumeratore,
+		 * se non matcha con nessuna della possibilita mi torna un eccezione di tipo IllegalArgumentException
+		 */
+		return Enum.valueOf(TipoTerreno.class,  input.toUpperCase());  
 	}
 	
-	
+	/**
+	 * Override del metodo toString.
+	 * Lo scopo e' stampare il valore dell'enumeratore in minuscolo
+	 */
 	@Override
-	//il metodo ritorna il valore dell'enum in minuscolo
 	public String toString(){
 		return super.toString().toLowerCase();
 	}
-
-
+	
+	/**
+	 * @return Vettore con tutti i possibili valori che puo' assumere l'enumeratore TipoTerreno
+	 */
+	public static TipoTerreno[] getTipi(){
+		return TipoTerreno.class.getEnumConstants();
+	}
+	
 }
